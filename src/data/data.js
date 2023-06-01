b83be962c85ee7eab3c9a03e1a448b1b7e5aacce
Žiga Šolar <zigasolarcs@gmail.com>
2023-06-01 11:02:36 +0200

    Added data

---
+++ src/data/data.js
@@ -0,0 +1,149 @@
+const oneSlider  = {
+    "sliderArray": [
+        {
+            "container":"Transportation",
+            "color":"#444389",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        }
+    ]
+
+}
+
+const twoSliders = {
+    "sliderArray": [
+        {
+            "container":"Transportation",
+            "color":"#444389",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        },
+        {
+            "container":"Food",
+            "color":"#1CCAD8",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        }
+    ]
+
+}
+
+const threeSliders = {
+    "sliderArray": [
+        {
+            "container":"Transportation",
+            "color":"#444389",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        },
+        {
+            "container":"Food",
+            "color":"#1CCAD8",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        },
+        {
+            "container":"Insurance",
+            "color":"#1EAE4E",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        }
+    ]
+
+}
+
+const fourSliders = {
+    "sliderArray": [
+        {
+            "container":"Transportation",
+            "color":"#444389",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        },
+        {
+            "container":"Food",
+            "color":"#1CCAD8",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        },
+        {
+            "container":"Insurance",
+            "color":"#1EAE4E",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        },
+        {
+            "container":"Entertainment",
+            "color":"#FFEC51",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        }
+    ]
+
+}
+
+const fiveSliders = {
+    "sliderArray": [
+        {
+            "container":"Transportation",
+            "color":"#444389",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        },
+        {
+            "container":"Food",
+            "color":"#1CCAD8",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        },
+        {
+            "container":"Insurance",
+            "color":"#1EAE4E",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        },
+        {
+            "container":"Entertainment",
+            "color":"#FFEC51",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        },
+        {
+            "container":"Health care",
+            "color":"#FF674D",
+            "max-val":10,
+            "min-val":0,
+            "step": 0,
+            "radius": 0
+        }
+    ]
+
+}
